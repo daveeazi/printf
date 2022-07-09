2ad5647e2fd4c86a7f7d82579a99a861d6106fa9
Alphydoo <aaoshiole@yahoo.com>
2022-07-09 16:00:26 +0100

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,51 +1,38 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
- */
+* _printf - Prints formated text.
+* @format: String with format of parameters.
+* Return: Length of text printed.
+*/
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int i = 0, c = 0;
+	va_list list;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	if (format)
 	{
-		if (format[i] == '%')
+		va_start(list, format);
+		while (format[i])
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			if (format[i] == '%')
+			{
+				i++;
+				if (!format[i])
+				{
+					return (-1);
+				}
+				if (conv(format, &i, &c, &list))
+				{
+					i++;
+					continue;
+				}
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			write(1, format + i++, 1);
+			c++;
 		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		return (c);
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	return (-1);
 }
