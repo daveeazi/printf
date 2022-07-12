fd3de690fdc9126d3253bfce3e3e4e8009bd7200
David <davidatat8@gmail.com>
2022-07-12 09:06:31 -0700

    Test Project

--- _printf.c
+++ _printf.c
@@ -1,87 +1,58 @@
 #include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
-<<<<<<< HEAD
-/**
-* _printf - Prints formated text.
-* @format: String with format of parameters.
-* Return: Length of text printed.
-*/
-int _printf(const char *format, ...)
-{
-	int i = 0, c = 0;
-	va_list list;
-
-	if (format)
-=======
-#include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * _printf - Build out the printf function
- * @format: string passed with possible format specifiers
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	int i, blen, hlen;
-	double totalBuffer;
-	double *total;
-	va_list argp;
-	char buffer[BUFSIZE], *holder;
-	char *(*pointer_get_valid)(va_list);
 
-	for (i = 0; i < BUFSIZE; i++)
-	{
-		buffer[i] = 0;
-	}
-	totalBuffer = 0;
-	pointer_get_valid = NULL;
-	total = &totalBuffer;
-	va_start(argp, format);
-	for (i = blen = hlen = 0; format && format[i]; i++)
->>>>>>> 5479b976e0906b71455f3d6a4faeb783d5132e03
-	{
-		va_start(list, format);
-		while (format[i])
-		{
-<<<<<<< HEAD
-			if (format[i] == '%')
-			{
-				i++;
-				if (!format[i])
-				{
-					return (-1);
-				}
-				if (conv(format, &i, &c, &list))
-				{
-					i++;
-					continue;
-				}
-			}
-			write(1, format + i++, 1);
-			c++;
-		}
-		return (c);
-	}
-	return (-1);
-=======
-			pointer_get_valid = get_valid_type(format[i + 1]);
-			holder = (pointer_get_valid == NULL) ?
-				found_nothing(format[i + 1]) :
-				pointer_get_valid(argp);
-			hlen = _strlen(holder);
-			blen = alloc_buffer(holder, hlen, buffer, blen, total);
-			i++;
-		}
-		else
-		{
-			holder = ctos(format[i]);
-			blen = alloc_buffer(holder, 1, buffer, blen, total);
-		}
-	}
-	va_end(argp);
-	_puts(buffer, blen);
-	return (totalBuffer + blen);
->>>>>>> 5479b976e0906b71455f3d6a4faeb783d5132e03
+int _printf(const char *format, ...){
+  int count = 0, i;
+  
+  va_list data;
+  va_start(data, format);
+
+  /* _printf("%s", 'Hello') */
+  
+  for (i = 0; format[i] != '\0'; ){
+    
+    /* count the number of characters */
+    /* print to the screen each character counted */
+    if (format[i] != '%'){
+      count += _putchar(format[i]);
+      i++;
+    }
+    else if (format[i] == '%' && format[i+1] !=' '){
+      switch (format[i + 1]){
+        case 'c':
+            /* print the character from the va_arguments */
+            count += _putchar(va_arg(data, int));
+            break;
+        case 's':
+            count += print_string(va_arg(data, char *));
+            break;
+        case '%':
+            /* print the character from the va_arguments */
+            count += _putchar('%');
+            break;
+        case 'd':
+            count += print_decimal(va_arg(data, int));
+            break;
+        case 'i':
+            count += print_decimal(va_arg(data, int));
+            break;
+        case 'b':
+            count += print_binary(va_arg(data, int));
+            break;
+        case 'u':
+            count += print_unsigned(va_arg(data, unsigned int));
+            break;
+        default:
+            break;
+      }
+      
+      i += 2;
+    }
+    
+    
+  }
+  
+
+  return (count);
 }
